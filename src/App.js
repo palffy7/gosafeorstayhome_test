95e37ca1df7544f30b0d30fd0381955013be01ee
palffy7 <ralph.amhof@hotmail.com>
2020-07-04 16:07:05 +0200

    form validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import './App.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGlassCheers } from '@fortawesome/free-solid-svg-icons';
-//import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 // import { useCookies } from 'react-cookie';
-// import { useFetch } from './hooks/useFetch';
 import Amplify, { Auth } from 'aws-amplify';
-import { withAuthenticator, UsernameAttributes } from 'aws-amplify-react';
 // import QRCode from 'qrcode.react'
 import { ReactComponent as Man } from './components/svgs/man.svg';
 import { ReactComponent as Woman } from './components/svgs/woman.svg';
@@ -58,7 +55,7 @@ function App() {
 
   //const [token, , deleteToken] = useCookies(['mr-token'])
   //const [data, loading, error] = useFetch();
-  const [signedUp, setSignedUp] = useState(true);
+  const [signedUp, setSignedUp] = useState(false);
   const [signeUpRequested, setSigneUpRequested] = useState(false);
   const [confirmed, setConfirmed] = useState(false)
   const [signedIn, setSignedIn] = useState(false);
@@ -82,14 +79,26 @@ function App() {
   const [stringQRCode, setStringQRCode] = useState(null)
   const [generatingQRCode, setGeneratingQRCode] = useState(true)
 
+  // triggers the data after its stored, otherwise user is empty
   useEffect(() => {
-    console.log(user)
     if (signedIn & confirmed) { GetUserAttributes() }
   }, [signedIn, confirmed, GetUserAttributes])
 
+  // triggers a new content for the QRCode for gerenating
   useEffect(() => {
     if (stringQRCode) { setGeneratingQRCode(false) }
   }, [stringQRCode])
+
+  useEffect(() => {
+    document.getElementById('login').reset()
+    if (userData['email']) {
+      if (!signedUp) {
+        SignUp()
+      } else {
+        SignIn()
+      }
+    }
+  }, [userData])
 
   // const loginClicked = () => {
   //   window.location.href = '/login';
@@ -101,8 +110,8 @@ function App() {
 
   const loginClicked = (e) => {
     console.log(userData)
-    console.log(user)
-    console.log(Auth.currentAuthenticatedUser())
+    // console.log(user)
+    // console.log(Auth.currentAuthenticatedUser())
     // GetUserAttributes()
 
     // var attr = Auth.userAttributes(user)
@@ -110,17 +119,33 @@ function App() {
     // attr.then(attr.json())
   }
 
+  const test = () => {
+    console.log('test')
+  }
+
   const handleChange = e => {
     const newUserData = userData;
     newUserData[e.target.name] = e.target.value
     setUserData(newUserData);
+    console.log(userData)
   }
 
   const handleVerificationCode = e => {
     setVerificationCode(e.target.value);
   }
 
+  const handleFormikSubmit = (values) => {
+    setUserData(values);
+    // document.getElementById('login').reset()
+    // if (!signedUp) {
+    //   SignUp()
+    // } else {
+    //   SignIn()
+    // }
+  }
+
   const handleSubmit = e => {
+    console.log('submit')
     e.preventDefault();
     document.getElementById('login').reset()
     if (!signedUp) {
@@ -142,6 +167,8 @@ function App() {
   }
 
   async function SignUp() {
+    console.log('signup')
+    console.log(userData)
     try {
       var respuser = await Auth.signUp({
         username: userData['email'],
@@ -173,7 +200,7 @@ function App() {
     } catch (error) {
       console.log('error confirming sign up', error);
     }
-    
+
   }
 
   async function SignIn() {
@@ -233,20 +260,122 @@ function App() {
             {!signedUp ?
               <div className='login'>
                 {!signeUpRequested ?
-                  <form id='login' onSubmit={handleSubmit}>
-                    <label>Vorname</label><br />
-                    <input type='text' name='given_name' onChange={handleChange} /><br />
-                    <label>Name</label><br />
-                    <input type='text' name='family_name' onChange={handleChange} /><br />
-                    <label>Password</label><br />
-                    <input type='password' name='password' onChange={handleChange} /><br />
-                    <label>Email</label><br />
-                    <input type='text' name='email' onChange={handleChange} /><br />
-                    <label>Phone Number</label><br />
-                    <input type='tel' name='phone_number' onChange={handleChange} /><br />
-                    <button>Sign up</button><br />
-                    <p onClick={() => setSignedUp(true)}>Du hast bereits ein Konto? Melde dich hier an!</p>
-                  </form>
+                  <React.Fragment>
+                    <Formik
+                      initialValues={{ given_name: '', family_name: '', password: '', email: '', phone_number: '' }}
+                      onSubmit={(values, { setSubmitting }) => {
+                        console.log('formik submitting')
+                        handleFormikSubmit(values)
+                      }}
+
+                      validationSchema={Yup.object().shape({
+                        family_name: Yup.string()
+                          .required('Required'),
+                        given_name: Yup.string()
+                          .required('Required'),
+                        email: Yup.string()
+                          .email()
+                          .required('Required'),
+                        password: Yup.string()
+                          .required('Required')
+                          .min(8, 'Passwort is too short, must be 8 characters long.')
+                          .matches(/(?=.*[0-9])/, 'Password must contain a number.')
+                          .matches(/(?=.*[a-z])/, 'Passwort muss einen Kleinbuchstaben enthalten.')
+                          .matches(/(?=.*[A-Z])/, 'Passwort muss einen Grossbuchstaben enthalten.'),
+                        phone_number: Yup.string()
+                          .required('Required')
+                          .matches(/(?=.*[+])/, 'Deine Nummer muss ein + enthalten.'),
+                      })}
+                    >
+                      {props => {
+                        const {
+                          values,
+                          touched,
+                          errors,
+                          isSubmitting,
+                          handleChange,
+                          handleBlur,
+                          handleSubmit
+                        } = props;
+                        return (
+                          <form id='login' onSubmit={handleSubmit}>
+                            <label>Vorname</label><br />
+                            <input
+                              type='text'
+                              name='given_name'
+                              placeholder='Gib hier deinen Vornamen ein'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={errors.given_name && touched.given_name && "error"} />
+                            {errors.given_name && touched.given_name && (
+                              <div className="input-feedback">{errors.given_name}</div>
+                            )}
+                            <label>Nachname</label><br />
+                            <input
+                              type='text'
+                              name='family_name'
+                              placeholder='Gib deinen Nachnamen ein'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={errors.family_name && touched.family_name && "error"} />
+                            {errors.family_name && touched.family_name && (
+                              <div className="input-feedback">{errors.family_name}</div>
+                            )}
+                            <label>Password</label><br />
+                            <input
+                              type='password'
+                              name='password'
+                              placeholder='Gib dein Passwort ein'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={errors.password && touched.password && "error"} />
+                            {errors.password && touched.password && (
+                              <div className="input-feedback">{errors.password}</div>
+                            )}
+                            <label>Email</label><br />
+                            <input
+                              type='text'
+                              name='email'
+                              placeholder='Gib deine Email Adresse ein'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={errors.email && touched.email && "error"} />
+                            {errors.email && touched.email && (
+                              <div className="input-feedback">{errors.email}</div>
+                            )}
+                            <label>Phone Number</label><br />
+                            <input
+                              type='tel'
+                              name='phone_number'
+                              placeholder='Gib deine Telefonnummer ein im Format +...'
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={errors.phone_number && touched.phone_number && "error"} />
+                            {errors.phone_number && touched.phone_number && (
+                              <div className="input-feedback">{errors.phone_number}</div>
+                            )}
+                            <button type='submit' disabled={isSubmitting}>Sign up</button><br />
+                            <p onClick={() => setSignedUp(true)}>Du hast bereits ein Konto? Melde dich hier an!</p>
+                          </form>
+                        )
+                      }}
+                    </Formik>
+
+                    {/* <form id='login' onSubmit={handleSubmit}>
+                      <label>Vorname</label><br />
+                      <input type='text' name='given_name' onChange={handleChange} /><br />
+                      <label>Name</label><br />
+                      <input type='text' name='family_name' onChange={handleChange} /><br />
+                      <label>Password</label><br />
+                      <input type='password' name='password' onChange={handleChange} /><br />
+                      <label>Email</label><br />
+                      <input type='text' name='email' onChange={handleChange} /><br />
+                      <label>Phone Number</label><br />
+                      <input type='tel' name='phone_number' onChange={handleChange} /><br />
+                      <button>Sign up</button><br />
+                      <p onClick={() => setSignedUp(true)}>Du hast bereits ein Konto? Melde dich hier an!</p>
+                    </form> */}
+                  </React.Fragment>
                   :
                   <form id='login' onSubmit={handleConfirm}>
                     <label>Type in Code</label><br />
@@ -356,7 +485,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
