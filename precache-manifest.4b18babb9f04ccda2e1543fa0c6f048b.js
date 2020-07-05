ad63070653ba8c140a7b5b2f704e9aff28fb92dc
palffy7 <ralph.amhof@hotmail.com>
2020-07-05 23:06:43 +0200

    Updates

---
+++ precache-manifest.4b18babb9f04ccda2e1543fa0c6f048b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d32e3706124158f88f9605119d2b113",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b1545f4f1abe2534590",
+    "url": "./static/css/main.b6d268dd.chunk.css"
+  },
+  {
+    "revision": "7cec721f1cb6cdf99240",
+    "url": "./static/js/2.372a7467.chunk.js"
+  },
+  {
+    "revision": "b2ed4752317c713da7a8cac084ac4b9b",
+    "url": "./static/js/2.372a7467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1545f4f1abe2534590",
+    "url": "./static/js/main.a0b65d23.chunk.js"
+  },
+  {
+    "revision": "778a8ed1a211fb9c37f7",
+    "url": "./static/js/runtime-main.867f120b.js"
+  },
+  {
+    "revision": "61027cfd54f70d53ea52527af9b85e56",
+    "url": "./static/media/accept.61027cfd.svg"
+  },
+  {
+    "revision": "ec2a58901094ff022574f41d85460c8e",
+    "url": "./static/media/arrow.ec2a5890.svg"
+  },
+  {
+    "revision": "ab4bcf77fba4d0801f09f5fb5a0360a4",
+    "url": "./static/media/ballon.ab4bcf77.svg"
+  },
+  {
+    "revision": "c475a6e7911f16b1e33b93da7de3c2cd",
+    "url": "./static/media/check.c475a6e7.svg"
+  },
+  {
+    "revision": "4e3cbdafd631e3aeb394e14c178ae624",
+    "url": "./static/media/login.4e3cbdaf.svg"
+  },
+  {
+    "revision": "d315fbcb530750ea0a84499717e876fa",
+    "url": "./static/media/logoPage.d315fbcb.svg"
+  },
+  {
+    "revision": "b60081b9581c8e681095d12b924e548c",
+    "url": "./static/media/man.b60081b9.svg"
+  },
+  {
+    "revision": "a73b772a3e97efc41eee70b288802a86",
+    "url": "./static/media/qrcode.a73b772a.svg"
+  },
+  {
+    "revision": "2cc721ea91b73b41270f591be9bd7ec8",
+    "url": "./static/media/stars.2cc721ea.svg"
+  },
+  {
+    "revision": "449084cd85d2f88d247a96552643c391",
+    "url": "./static/media/woman.449084cd.svg"
+  }
+]);
